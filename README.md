91b96e6f1ded5d69a9163258849b7d7eaeba0a82
Dan Book <grinnz@grinnz.com>
2015-06-23 17:01:56 -0400

    documentation module and dist setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# Perl-Critic-Policy-Freenode
-Perl::Critic::Policy::Freenode - Perl::Critic policies inspired by #perl on freenode IRC
---
+++ lib/Perl/Critic/Policy/Freenode.pm
@@ -0,0 +1,47 @@
+package Perl::Critic::Policy::Freenode;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+Perl::Critic::Policy::Freenode - Perl::Critic policies inspired by #perl on
+freenode IRC
+
+=head1 AFFILIATION
+
+This module has no functionality, but instead contains documentation for this
+distribution and acts as a means of pulling other modules into a bundle. All of
+the Policy modules contained herein will have an "AFFILIATION" section
+announcing their participation in this grouping.
+
+=head1 DESCRIPTION
+
+A set of L<Perl::Critic> policies to enforce the practices generally considered
+acceptable by the denizens of #perl on L<Freenode IRC|https://freenode.net/>.
+
+=head1 POLICIES
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+All policies included are in the "freenode" theme. See the L<Perl::Critic>
+documentation for how to make use of this.
+
+=head1 AUTHOR
+
+Dan Book, C<dbook@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2015, Dan Book.
+
+This library is free software; you may redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=head1 SEE ALSO
+
+L<Perl::Critic>
