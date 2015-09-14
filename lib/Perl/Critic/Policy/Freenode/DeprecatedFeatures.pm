49a8f7e74046f62ba505bbaf9ede94fe71cd9745
Dan Book <grinnz@grinnz.com>
2015-09-14 18:13:19 -0400

    some more deprecated features (#16)

--- lib/Perl/Critic/Policy/Freenode/DeprecatedFeatures.pm
+++ lib/Perl/Critic/Policy/Freenode/DeprecatedFeatures.pm
@@ -12,11 +12,17 @@ our $VERSION = '0.011';
 sub supported_parameters { () }
 sub default_severity { $SEVERITY_HIGH }
 sub default_themes { 'freenode' }
-sub applies_to { 'PPI::Token' }
+sub applies_to { 'PPI::Element' }
 
 my %features = (
 	'$[' => {
 		expl => 'Use of $[ is deprecated in perl v5.12.0. See Array::Base and String::Base.',
+	},
+	':=' => {
+		expl => 'Use of := as an empty attribute list is deprecated in perl v5.12.0, use = alone.',
+	},
+	'UNIVERSAL->import()' => {
+		expl => 'The method UNIVERSAL->import() (or passing import arguments to "use UNIVERSAL") is deprecated in perl v5.12.0.',
 	},
 );
 
@@ -30,8 +36,30 @@ sub _violation {
 
 sub violates {
 	my ($self, $elem) = @_;
-	if ($elem->isa('PPI::Token::Symbol') and $elem eq '$[') {
-		return $self->_violation('$[', $elem);
+	my $next;
+	my @args;
+	if ($elem->isa('PPI::Statement')) {
+		if ($elem->isa('PPI::Statement::Include') and $elem->type eq 'use'
+		         and $elem->module eq 'UNIVERSAL' and @args = $elem->arguments
+		         and (!$args[0]->isa('PPI::Structure::List') or $args[0]->schildren)) {
+			return $self->_violation('UNIVERSAL->import()', $elem);
+		}
+	} elsif ($elem->isa('PPI::Token')) {
+		if ($elem->isa('PPI::Token::Symbol')) {
+			if ($elem eq '$[') {
+				return $self->_violation('$[', $elem);
+			}
+		} elsif ($elem->isa('PPI::Token::Operator')) {
+			if ($elem eq ':' and $next = $elem->next_sibling and $next->isa('PPI::Token::Operator') and $next eq '=') {
+				return $self->_violation(':=', $elem);
+			}
+		} elsif ($elem->isa('PPI::Token::Word')) {
+			if ($elem eq 'UNIVERSAL'
+		        and $next = $elem->snext_sibling and $next->isa('PPI::Token::Operator') and $next eq '->'
+		        and $next = $next->snext_sibling and $next->isa('PPI::Token::Word') and $next eq 'import') {
+				return $self->_violation('UNIVERSAL->import()', $next);
+			}
+		}
 	}
 	return ();
 }
@@ -62,6 +90,20 @@ L<arybase>.pm in v5.16.0, and it is essentially a synonym for C<0> under
 C<use v5.16> or C<no feature "array_base">. While it is probably a bad idea in
 general, the modules L<Array::Base> and L<String::Base> can now be used to
 replace this functionality.
+
+=head2 :=
+
+Because the whitespace between an attribute list and assignment operator is not
+significant, it was possible to specify assignment to a variable with an empty
+attribute list with a construction like C<my $foo := 'bar'>. This was
+deprecated in perl v5.12.0 to allow the possibility of a future C<:=> operator.
+Avoid the issue by either putting whitespace between the C<:> and C<=>
+characters or simply omitting the empty attribute list.
+
+=head2 UNIVERSAL->import()
+
+The method C<UNIVERSAL->import()> and similarly passing import arguments to
+C<use UNIVERSAL> was deprecated in perl v5.12.0.
 
 =head1 AFFILIATION
 
