1d5b0d996cf0be12409b31741a3e1a3eaae4d71d
Dan Book <grinnz@grinnz.com>
2018-06-20 20:11:12 -0400

    add LoopOnHash policy

--- lib/Perl/Critic/Freenode.pm
+++ lib/Perl/Critic/Freenode.pm
@@ -84,6 +84,10 @@ Don't use C<return> with no arguments
 
 Don't call methods indirectly
 
+=item L<Perl::Critic::Policy::Freenode::LoopOnHash>
+
+Don't loop over hashes
+
 =item L<Perl::Critic::Policy::Freenode::ModPerl>
 
 Don't use C<mod_perl> to write web applications
