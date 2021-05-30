d91f12ecae736cad5971cbe1977af8f34347df5d
Dan Book <grinnz@grinnz.com>
2021-05-29 20:54:42 -0400

    mention CGI::Tiny and Syntax::Keyword::Match as alternatives to discouraged modules

--- lib/Perl/Critic/Policy/Community/DiscouragedModules.pm
+++ lib/Perl/Critic/Policy/Community/DiscouragedModules.pm
@@ -17,7 +17,7 @@ my %modules = (
 	'AnyEvent' => 'AnyEvent\'s author refuses to use public bugtracking and actively breaks interoperability. POE, IO::Async, and Mojo::IOLoop are widely used and interoperable async event loops.',
 	'Any::Moose' => 'Any::Moose is deprecated. Use Moo instead.',
 	'Class::DBI' => 'Class::DBI is an ancient database ORM abstraction layer which is buggy and abandoned. See DBIx::Class for a more modern DBI-based ORM, or Mad::Mapper for a Mojolicious-style ORM.',
-	'CGI' => 'CGI.pm is an ancient module for communicating via the CGI protocol, with tons of bad practices and cruft. Use a modern framework such as those based on Plack (Web::Simple, Dancer2, Catalyst) or Mojolicious, they can still be served via CGI if you choose.',
+	'CGI' => 'CGI.pm is an ancient module for communicating via the CGI protocol, with tons of bad practices and cruft. Use a modern framework such as those based on Plack (Web::Simple, Dancer2, Catalyst) or Mojolicious, they can still be served via CGI if you choose. Use CGI::Tiny if you are limited to the CGI protocol.',
 	'Coro' => 'Coro abuses Perl internals in an unsupported way. Consider Future and Future::AsyncAwait in combination with event loops for similar semantics.',
 	'Error' => 'Error.pm is overly magical and discouraged by its maintainers. Try Throwable for exception classes in Moo/Moose, or Exception::Class otherwise. Try::Tiny or Syntax::Keyword::Try are recommended for the try/catch syntax.',
 	'File::Slurp' => 'File::Slurp gets file encodings all wrong, line endings on win32 are messed up, and it was written before layers were properly added. Use File::Slurper, Path::Tiny, Data::Munge, or Mojo::File.',
@@ -27,7 +27,7 @@ my %modules = (
 	'JSON::Any' => 'JSON::Any is deprecated. Use JSON::MaybeXS instead.',
 	'JSON::XS' => 'JSON::XS\'s author refuses to use public bugtracking and actively breaks interoperability. Cpanel::JSON::XS is a fork with several bugfixes and a more collaborative maintainer. See also JSON::MaybeXS.',
 	'Net::IRC' => 'Net::IRC is an ancient module implementing the IRC protocol. Use a modern event-loop-based module instead. Choices are POE::Component::IRC (and Bot::BasicBot based on that), Net::Async::IRC, and Mojo::IRC.',
-	'Switch' => 'Switch.pm is a buggy and outdated source filter which can cause any number of strange errors, in addition to the problems with smart-matching shared by its replacement, the \'switch\' feature (given/when). Try Switch::Plain instead.',
+	'Switch' => 'Switch.pm is a buggy and outdated source filter which can cause any number of strange errors, in addition to the problems with smart-matching shared by its replacement, the \'switch\' feature (given/when). Try Switch::Plain or Syntax::Keyword::Match instead.',
 	'XML::Simple' => 'XML::Simple tries to coerce complex XML documents into perl data structures. This leads to overcomplicated structures and unexpected behavior. Use a proper DOM parser instead like XML::LibXML, XML::TreeBuilder, XML::Twig, or Mojo::DOM.',
 );
 
@@ -82,7 +82,8 @@ modern L<DBI>-based ORM, or L<Mad::Mapper> for a L<Mojolicious>-style ORM.
 L<CGI>.pm is an ancient module for communicating via the CGI protocol, with
 tons of bad practices and cruft. Use a modern framework such as those based on
 L<Plack> (L<Web::Simple>, L<Dancer2>, L<Catalyst>) or L<Mojolicious>, they can
-still be served via CGI if you choose.
+still be served via CGI if you choose. Use L<CGI::Tiny> if you are limited to
+the CGI protocol.
 
 =head2 Coro
 
@@ -148,7 +149,7 @@ L<Bot::BasicBot>), L<Net::Async::IRC>, and L<Mojo::IRC>.
 L<Switch>.pm is a buggy and outdated source filter which can cause any number
 of strange errors, in addition to the problems with smart-matching shared by
 its replacement, L<feature/"The 'switch' feature"> (C<given>/C<when>). Try
-L<Switch::Plain> instead.
+L<Switch::Plain> or L<Syntax::Keyword::Match> instead.
 
 =head2 XML::Simple
 
